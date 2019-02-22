e50b8cc96dcb5319a5ef4c0368aeb8c1a46a88c1
Michał Sochoń <michal.sochon@codilime.com>
2019-02-22 22:29:09 +0100

    Ensure shell scripts pass shellcheck, and fail fast

--- tools/loaddata.sh
+++ tools/loaddata.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+set -xe
 python ../manage.py loaddata user.json
 python ../manage.py loadtestdata meals.Restaurant:10 meals.MealCategory:10 meals.Meal:10 users.Location:10 meals.UserOrder:10
